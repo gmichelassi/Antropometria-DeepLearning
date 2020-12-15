0eb4cc65af34d21e85ffffbba3b78d989c5f43df
Gabriel <gabrielmichelassi@usp.br>
2020-12-15 16:45:09 -0300

    Adiciona reconhecimento dos olhos dentro apos encontrar a face

--- main.py
+++ main.py
@@ -20,13 +20,23 @@ def ResizeWithAspectRatio(image, width=None, height=None, inter=cv2.INTER_AREA):
 def load_image():
 	img = cv2.imread('img/MIT-10.jpg')
 
-	gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 	face_cascade = cv2.CascadeClassifier('classifiers/haarcascade_frontalface_alt2.xml')
+	eye_cascade = cv2.CascadeClassifier('classifiers/haarcascade_eye.xml')
+
 	# parâmetros abaixo são: scale factor, min_neighbors, scale_image e min_size
+	gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 	faces = face_cascade.detectMultiScale(gray_img, 1.3, 5, cv2.CASCADE_SCALE_IMAGE, (20, 20))
 
-	for (x, y, w, h) in faces:
-		cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+	for (x, y, width, height) in faces:
+		cv2.rectangle(img, (x, y), (x + width, y + height), (255, 0, 0), 2)
+
+		# roi stands for "region of interest"
+		roi_gray = gray_img[y:y + height, x: x + width]
+		roi_colorful = img[y:y + height, x: x + width]
+
+		eyes = eye_cascade.detectMultiScale(roi_gray)
+		for (eye_x, eye_y, eye_width, eye_height) in eyes:
+			cv2.rectangle(roi_colorful, (eye_x, eye_y), (eye_x + eye_width, eye_y + eye_height), (0, 255, 0), 2)
 
 	resized_img = ResizeWithAspectRatio(img, width=500)
 	cv2.imshow("Image", resized_img)
