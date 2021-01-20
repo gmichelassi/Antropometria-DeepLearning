bfc1558b6aa3b86a26051efbd0cf99b2981a2fea
Gabriel <gabrielmichelassi@usp.br>
2021-01-20 10:49:50 -0300

    Inicia construção do pipeline da rede neural

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,5 +1,10 @@
 import cv2
 import os
+import numpy as np
+import tensorflow as tf
+from keras.engine import Model
+from keras_vggface.vggface import VGGFace
+from keras.layers import Dropout, Flatten, Dense
 from glob import glob
 from config import config as cfg
 from config import constants as cte
@@ -21,9 +26,30 @@ def loadData(img_folder):
 	return X, y
 
 
-if __name__ == '__main__':
-	casos 		= cfg.CROPPED + cfg.CASOS + cfg.DSCN_MASK
-	controles 	= cfg.CROPPED + cfg.CONTROLES + cfg.DSCN_MASK
+def main():
+	casos = cfg.CROPPED + cfg.CASOS + cfg.DSCN_MASK
+	controles = cfg.CROPPED + cfg.CONTROLES + cfg.DSCN_MASK
 
 	X, y = loadData([casos, controles])
-	print(len(X), len(y))
+
+	vgg_model = VGGFace(include_top=False, input_shape=(584, 584, 3), pooling='max')
+	last_layer = vgg_model.get_layer('pool5').output
+	x = Dropout(.2, trainable=False, name='custom_dropout1')(last_layer)
+	x = Flatten(name='flatten')(x)
+	x = Dense(16, activation='relu', name='custom_fc1')(x)
+	x = Dropout(.2, trainable=False, name='custom_dropout_2')(x)
+	x = Dense(16, activation='relu', name='custom_fc2')(x)
+	out = Dense(16, activation='relu', name='custom_fc3')(x)
+	custom_vgg_model = Model(vgg_model.input, out)
+
+	custom_vgg_model.compile(optimizer='', loss='', metrics=['accuracy'])
+
+	model.fit(X, y, epochs=10)
+
+	test_loss, test_acc = model.evaluate(X, y, verbose=2)
+
+	print('\nTest accuracy:', test_acc)
+
+
+if __name__ == '__main__':
+	main()
