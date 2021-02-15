1932546fa3e6460bc342cacc49ef9d01da1700ce
Gabriel <gabrielmichelassi@usp.br>
2021-02-15 16:18:32 -0300

    Finaliza algoritmo de rescale para tratar imagens cujo primeiro processamento foi ruim

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -14,6 +14,7 @@ from utils.utils import handleError
 from utils.imageProcessing import cropImage, resizeWithAspectRatio, rotateImage, findEyeCoordinates
 
 log = logger.getLogger(__file__)
+crop_dimension = (584, 584)
 
 
 def faceSize(image_folder):
@@ -64,7 +65,32 @@ def faceSize(image_folder):
 	}
 
 
-def preprocessImage(img_folder, crop_width, crop_height, show_result=False):
+def rescaleAndProcessImage(images):
+	for image_path in images:
+		try:
+			original_image = cv2.imread(image_path)
+			(h, w) = original_image.shape[:2]
+
+			img_absolute_path, img_name = os.path.split(image_path)
+			resized_path = img_absolute_path + '/rescaled_' + img_name
+
+			scale_factor = .9
+			isImageReady = False
+			while not isImageReady:
+				resized_image = resizeWithAspectRatio(original_image, int(w * scale_factor))
+				cv2.imwrite(resized_path, resized_image)
+				processImage(image_path=resized_path, original_image=resized_image, show_result=True)
+
+				r = input('Resultado do processamento foi o esperado (y/n): ')
+				if r == 'y':
+					isImageReady = True
+				else:
+					scale_factor = scale_factor * 0.9
+		except IOError as ioe:
+			handleError("Its was not possible to load image {0} due to error {1}".format(image, ioe))
+
+
+def defaultProcessing(img_folder):
 	log.info('Finding images')
 	images = []
 	for img_path in img_folder:
@@ -72,111 +98,114 @@ def preprocessImage(img_folder, crop_width, crop_height, show_result=False):
 
 	log.info('{0} images found'.format(len(images)))
 
+	for image_path in images:
+		original_image = cv2.imread(image_path)
+		processImage(image_path=image_path, original_image=original_image, show_result=True)
+
+
+def processImage(image_path, original_image, show_result=False):
 	log.info('Loading face and eye detectors')
 	face_cascade = cv2.CascadeClassifier('classifiers/haarcascade_frontalface_alt2.xml')
 	eye_cascade = cv2.CascadeClassifier('classifiers/haarcascade_eye.xml')
 
-	for image in images:
-		# Onde salvar as imagens
-		img_absolute_path, img_name = os.path.split(image)
-		path, label = os.path.split(img_absolute_path)
+	img_absolute_path, img_name = os.path.split(image_path)
+	path, label = os.path.split(img_absolute_path)
 
-		eyes_path = cfg.EYES + '/' + label + '/' + img_name
-		gradient_path = cfg.GRADIENT + '/' + label + '/' + img_name
-		rotated_path = cfg.ROTATED + '/' + label + '/' + img_name
-		cropped_path = cfg.CROPPED + '/' + label + '/' + img_name
+	eyes_path = cfg.EYES + '/' + label + '/' + img_name
+	gradient_path = cfg.GRADIENT + '/' + label + '/' + img_name
+	rotated_path = cfg.ROTATED + '/' + label + '/' + img_name
+	cropped_path = cfg.CROPPED + '/' + label + '/' + img_name
 
-		try:
-			original_image = cv2.imread(image)
-			gray_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+	try:
+		gray_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
 
-			log.info('Finding face for {0}'.format(img_name))
-			faces = face_cascade.detectMultiScale(gray_image, 1.3, 5, cv2.CASCADE_SCALE_IMAGE, (20, 20))
-			log.info("Found {0} faces!".format(len(faces)))
+		log.info('Finding face for {0}'.format(img_name))
+		faces = face_cascade.detectMultiScale(gray_image, 1.3, 5, cv2.CASCADE_SCALE_IMAGE, (20, 20))
+		log.info("Found {0} faces!".format(len(faces)))
 
-			if len(faces) == 0:
-				return
+		if len(faces) == 0:
+			return
 
-			x, y, width, height = faces[0]
-			cv2.rectangle(original_image, (x, y), (x + width, y + height), (255, 0, 0), 2)
+		x, y, width, height = faces[0]
+		cv2.rectangle(original_image, (x, y), (x + width, y + height), (255, 0, 0), 2)
 
-			# roi stands for "region of interest"
-			roi_gray = gray_image[y:y + height, x: x + width]
-			roi_colorful = original_image[y:y + height, x: x + width]
+		# roi stands for "region of interest"
+		roi_gray = gray_image[y:y + height, x: x + width]
+		roi_colorful = original_image[y:y + height, x: x + width]
 
-			# Recortar somente a face
-			found_image = Image.open(image)
-			detected_face = found_image.crop((x, y, x + width, y + height))
-			face = np.asarray(detected_face)
-			face_gray = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+		# Recortar somente a face
+		found_image = Image.open(image_path)
+		detected_face = found_image.crop((x, y, x + width, y + height))
+		face = np.asarray(detected_face)
+		face_gray = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
 
-			# Encontrar os olhos
-			log.info('Finding eye for {0}'.format(img_name))
-			eyes = eye_cascade.detectMultiScale(face_gray)
+		# Encontrar os olhos
+		log.info('Finding eye for {0}'.format(img_name))
+		eyes = eye_cascade.detectMultiScale(face_gray)
 
-			for (eye_x, eye_y, eye_width, eye_height) in eyes:
-				cv2.rectangle(roi_colorful, (eye_x, eye_y), (eye_x + eye_width, eye_y + eye_height), (0, 255, 0), 2)
-				eyes_pair = face_gray[eye_y:eye_y + eye_height, eye_x:eye_x + eye_width]
-				eyes_box = (eye_x, eye_y, eye_x + eye_width, eye_y + eye_height)
+		for (eye_x, eye_y, eye_width, eye_height) in eyes:
+			cv2.rectangle(roi_colorful, (eye_x, eye_y), (eye_x + eye_width, eye_y + eye_height), (0, 255, 0), 2)
+			eyes_pair = face_gray[eye_y:eye_y + eye_height, eye_x:eye_x + eye_width]
+			eyes_box = (eye_x, eye_y, eye_x + eye_width, eye_y + eye_height)
 
-			canny = cv2.Canny(eyes_pair, 50, 245)
-			kernel = np.ones((3, 3), np.uint8)
-			gradient = cv2.morphologyEx(canny, cv2.MORPH_GRADIENT, kernel)
+		canny = cv2.Canny(eyes_pair, 50, 245)
+		kernel = np.ones((3, 3), np.uint8)
+		gradient = cv2.morphologyEx(canny, cv2.MORPH_GRADIENT, kernel)
 
-			# find coordinates of the pupils
-			h = gradient.shape[0]
-			w = gradient.shape[1]
+		# find coordinates of the pupils
+		h = gradient.shape[0]
+		w = gradient.shape[1]
 
-			x1, y1 = findEyeCoordinates(gradient, h, 0, int(w / 2))  # left eye
-			x1, y1 = abs(x1), abs(y1)  # normalizar geometricamente
+		x1, y1 = findEyeCoordinates(gradient, h, 0, int(w / 2))  # left eye
+		x1, y1 = abs(x1), abs(y1)  # normalizar geometricamente
 
-			x2, y2 = findEyeCoordinates(gradient, h, int(w / 2), w)  # right eye
-			x2, y2 = abs(x2), abs(y2)
+		x2, y2 = findEyeCoordinates(gradient, h, int(w / 2), w)  # right eye
+		x2, y2 = abs(x2), abs(y2)
 
-			dx, dy = abs(x2 - x1), (abs(y2 - y1) * -1)
+		dx, dy = abs(x2 - x1), (abs(y2 - y1) * -1)
 
-			log.info('Finding angle to rotate image')
-			z = Decimal(dy) / Decimal(dx)
-			alpha_complex = cmath.atan(z)
-			alpha = cmath.phase(alpha_complex)
-			alpha = alpha / 2
+		log.info('Finding angle to rotate image')
+		z = Decimal(dy) / Decimal(dx)
+		alpha_complex = cmath.atan(z)
+		alpha = cmath.phase(alpha_complex)
+		alpha = alpha / 2
 
-			log.info('Angle found {0}'.format(alpha))
-			log.info('Rotating image...')
+		log.info('Angle found {0}'.format(alpha))
+		log.info('Rotating image...')
 
-			img_to_rotate = Image.open(image)
-			rotated_image = img_to_rotate.rotate(-alpha)
-			rotated_image.save(rotated_path)
-			image_rotated = cv2.imread(rotated_path)
+		img_to_rotate = Image.open(image_path)
+		rotated_image = img_to_rotate.rotate(-alpha)
+		rotated_image.save(rotated_path)
+		image_rotated = cv2.imread(rotated_path)
 
-			log.info('Cropping image around face...')
-			croppedImage = cropImage(image_rotated, x, y, width, height, crop_width, crop_height)
+		log.info('Cropping image around face...')
+		croppedImage = cropImage(image_rotated, x, y, width, height, crop_dimension[0], crop_dimension[1])
 
-			if show_result:
-				log.info('Showing result...')
-				resized_img = resizeWithAspectRatio(original_image, width=500)
-				cv2.imshow("Original Image", resized_img)
-				cv2.waitKey(3000)
+		if show_result:
+			log.info('Showing result...')
+			resized_original_img = resizeWithAspectRatio(original_image, width=500)
+			cv2.imshow("Original Image", resized_original_img)
+			cv2.waitKey(3000)
 
-				resized_img = resizeWithAspectRatio(croppedImage, width=500)
-				cv2.imshow("Processed Image", resized_img)
-				cv2.waitKey(3000)
+			resized_final_img = resizeWithAspectRatio(croppedImage, width=500)
+			cv2.imshow("Processed Image", resized_final_img)
+			cv2.waitKey(3000)
 
-				cv2.destroyAllWindows()
+			cv2.destroyAllWindows()
 
-			log.info('Preprocessing done for {0}, saving outputs'.format(img_name))
-			cv2.imwrite(eyes_path, eyes_pair)
-			cv2.imwrite(gradient_path, gradient)
-			cv2.imwrite(cropped_path, croppedImage)
+		log.info('Preprocessing done for {0}, saving outputs'.format(img_name))
+		cv2.imwrite(eyes_path, eyes_pair)
+		cv2.imwrite(gradient_path, gradient)
+		cv2.imwrite(cropped_path, croppedImage)
 
-		except ZeroDivisionError as zde:
-			handleError('It was not possible to preprocesses image {0} because of error {1}'.format(img_name, zde))
-		except ValueError as ve:
-			handleError('It was not possible to preprocesses image {0} because of error {1}'.format(img_name, ve))
-		except IOError as ioe:
-			handleError('It was not possible to preprocesses image {0} because of error {1}'.format(img_name, ioe))
-		except TypeError as te:
-			handleError('It was not possible to preprocesses image {0} because of error {1}'.format(img_name, te))
+	except ZeroDivisionError as zde:
+		handleError('It was not possible to preprocesses image {0} because of error {1}'.format(img_name, zde))
+	except ValueError as ve:
+		handleError('It was not possible to preprocesses image {0} because of error {1}'.format(img_name, ve))
+	except IOError as ioe:
+		handleError('It was not possible to preprocesses image {0} because of error {1}'.format(img_name, ioe))
+	except TypeError as te:
+		handleError('It was not possible to preprocesses image {0} because of error {1}'.format(img_name, te))
 
 
 if __name__ == '__main__':
@@ -198,4 +227,8 @@ if __name__ == '__main__':
 	all_images = [casos, controles, a22q11, angelman, apert, cdl, down, fragilex, marfan, progeria, sotos, treacher, turner, williams]
 	casos_controles_images = [casos, controles]
 
-	preprocessImage(casos_controles_images, 584, 584, True)
+	# defaultProcessing(casos_controles_images)
+	image_path = cfg.IMG_DIR + '/_testImage/MIT-10.jpg'
+	# original_image = cv2.imread(image_path)
+	# processImage(image_path=image_path, original_image=original_image, show_result=True)
+	rescaleAndProcessImage([image_path])
--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,9 @@
+from config import logger
+log = logger.getLogger(__file__)
+
+
 def handleError(msg):
 	log.info(msg)
-	f = open('../output/error.txt', 'a')
+	f = open('./output/error.txt', 'a')
 	f.write(msg + '\n')
 	f.close()
