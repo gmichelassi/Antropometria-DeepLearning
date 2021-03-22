2131a72fcaeff1b0680fb0cdbab276609e810f5c
Gabriel <gabrielmichelassi@usp.br>
2021-03-22 11:22:46 -0300

    Completa os parâmetros e configurações do classificador a serem testados

--- utils/classifier.py
+++ utils/classifier.py
@@ -7,8 +7,23 @@ from keras.layers import Dropout, Flatten, Dense, BatchNormalization
 class Classifier:
 	def __init__(self):
 		self.optimizers = [
-			SGD(learning_rate=0.01, momentum=0.0),
-			Adam(learning_rate=0.001)
+			SGD(learning_rate=0.001, 	momentum=0.0),
+			SGD(learning_rate=0.01, 	momentum=0.0),
+			SGD(learning_rate=0.1, 		momentum=0.0),
+			SGD(learning_rate=1, 		momentum=0.0),
+			SGD(learning_rate=10, 		momentum=0.0),
+			SGD(learning_rate=0.001, 	momentum=0.1),
+			SGD(learning_rate=0.01, 	momentum=0.1),
+			SGD(learning_rate=0.1, 	momentum=0.1),
+			SGD(learning_rate=1, 	momentum=0.1),
+			SGD(learning_rate=10, 	momentum=0.1),
+			Adam(learning_rate=0.0001),
+			Adam(learning_rate=0.001),
+			Adam(learning_rate=0.01),
+			Adam(learning_rate=0.1),
+			Adam(learning_rate=0.5),
+			Adam(learning_rate=1),
+			Adam(learning_rate=10)
 		]
 		self.losses = ['binary_crossentropy']
 		self.metrics = [Accuracy(), Precision(), Recall(), AUC()]
@@ -19,12 +34,12 @@ class Classifier:
 				Dropout(.2, trainable=False, name='custom_dropout1'),
 				Flatten(name='flatten'),
 				Dense(16, activation='relu', name='custom_fc1'),
-				Dropout(.2, trainable=False, name='custom_dropout_2'),
-				Dense(16, activation='relu', name='custom_fc2'),
+				Dropout(.5, trainable=False, name='custom_dropout_2'),
+				Dense(8, activation='relu', name='custom_fc2'),
 				Dense(1, activation='relu', name='custom_fc3')
 			],
 			[
-				Dense(16, activation='relu', name='custom_fc1'),
+				Dense(32, activation='relu', name='custom_fc1'),
 				BatchNormalization(),
 				Dense(16, activation='relu', name='custom_fc2'),
 				BatchNormalization(),
