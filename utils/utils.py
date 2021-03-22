69f111126e3f7f2198ac292cacee61ea5e2f55e2
Gabriel <gabrielmichelassi@usp.br>
2021-03-22 11:24:40 -0300

    Altera output de erros

--- utils/utils.py
+++ utils/utils.py
@@ -4,6 +4,6 @@ log = logger.getLogger(__file__)
 
 def handleError(msg):
 	log.info(msg)
-	f = open('./output/error.txt', 'a')
+	f = open('./output/error.log', 'a')
 	f.write(msg + '\n')
 	f.close()
